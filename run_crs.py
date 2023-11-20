3a8203221b93e90c0c4ee83161b6030e56f2e5a4
alcopeland <a.l.copeland@lancaster.ac.uk>
2023-11-20 11:31:47 +0000

    Changed how the dataset is structured, not quite working yet.

--- run_crs.py
+++ run_crs.py
@@ -1,91 +1,101 @@
-def read_booksummaries():
+import torch
+class BooksDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+def read_booksummaries(texts, labels):
     print("reading booksummaries")
     import re
-    open('datasets/uncompressed/total_text.txt', 'w').close()
-    open('datasets/uncompressed/cleanbooksummaries.txt', 'w').close()
-    with open("datasets/uncompressed/booksummaries.txt", "r", encoding="utf-8") as file, open("datasets/uncompressed/total_text.txt", "a", encoding="utf-8") as newFile, open("datasets/uncompressed/cleanbooksummaries.txt", "a", encoding="utf-8") as cleanFile:
+    with open("datasets/uncompressed/booksummaries.txt", "r", encoding="utf-8") as file:
         sep1 = re.compile(r'"/m/\w{4,7}":')
         sep2 = re.compile(r'"/m/\w{4,7}"')
         sep3 = re.compile(r'/m/\w{4,7}')
-        sep4 = re.compile(r'{')
-        sep5 = re.compile(r'}')
         for line in file.readlines():
             newLine = re.sub(sep1,'',line)
             newLine = re.sub(sep2,'',newLine)
             newLine = re.sub(sep3,'',newLine)
-            newLine = re.sub(sep4,'',newLine)
-            newLine = re.sub(sep5,'',newLine)
-            newLine = ' '.join(newLine.split())
-            newFile.write(f"{newLine} ")
-            cleanFile.write(f"{newLine} ")
+            split = newLine.split("{")
+            noNumber = split[0].split("\t\t")
+            if(len(split)>1):
+                genreSplit = split[1].split("}")
+                text = ' '.join((noNumber[1] + genreSplit[1]).split())
+                texts.append(text)
+                label = re.sub('"','',genreSplit[0])
+                label = label.split(",  ")
+                label[0] = label[0][1:]
+                labels.append(label)
 
-def read_booksdataset():
+def read_booksdataset(texts, labels):
     print("reading booksdataset")
     import csv
     with open("datasets/uncompressed/BooksDataSet.csv", "r", encoding="utf-8", errors="ignore") as file, open("datasets/uncompressed/total_text.txt", "a+", encoding="utf-8") as file2, open("datasets/uncompressed/cleanbooksdataset.txt", "w", encoding="utf-8") as cleanFile:
         csv_reader = csv.reader(file)
         next(file)
         for row in csv_reader:
-            text = f"{row[2]} {row[3]} {row[4]}"
-            file2.write(text)
-            cleanFile.write(text)
-    
-def read_books1():
-    print("reading all books")
-    import os
-    for filename in os.listdir("datasets/uncompressed/books1/epubtxt"):
-        read_book("datasets/uncompressed/books1/epubtxt/"+filename)
-
-def read_book(path):
-    with open(path, "r", encoding = "utf-8") as file, open("datasets/uncompressed/total_text.txt", "a+", encoding="utf-8") as file2:
-        book_text = file.read()
-        book_text = ' '.join(book_text.split())
-        file2.write(book_text)
+            text = f"{row[2]} {row[4]}"
+            texts.append(text)
+            labels.append(row[3])
 
 def read_datasets():
+    texts = []
+    labels = []
     print("reading datasets")
-    read_booksummaries()
-    read_booksdataset()
-    # read_books1()
+    # read_booksummaries(texts, labels)
+    read_booksdataset(texts, labels)
+    return texts, labels  
 
-    with open("datasets/uncompressed/total_text.txt", "r", encoding="utf-8") as file:
-        total_text = file.read()
-        training_ratio = int(0.8 * len(total_text))
-        training_data = total_text[:training_ratio]
-        validation_data = total_text[training_ratio:]
-    with open("datasets/uncompressed/training_data.txt", "w", encoding="utf-8") as f:
-        f.write(training_data)
-    with open("datasets/uncompressed/validation_data.txt", "w", encoding="utf-8") as f:
-        f.write(validation_data)    
-
-def train_bot():
+def train_bot(texts, labels):
     print("training model")    
-    import torch
-    from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-    model = GPT2LMHeadModel.from_pretrained('gpt2-large', pad_token_id = tokenizer.eos_token_id)
+    ratio = int(len(texts)*0.8)
+    training_texts = texts[:ratio]
+    testing_texts = texts[ratio:]
+    training_labels = labels[:ratio]
+    testing_labels = labels[ratio:]
     
-    training_dataset = TextDataset(tokenizer=tokenizer, file_path="datasets/uncompressed/training_data.txt", block_size=128)
-    validation_dataset = TextDataset(tokenizer=tokenizer, file_path="datasets/uncompressed/validation_data.txt", block_size=128)
-    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    from sklearn.model_selection import train_test_split
+    training_texts, validation_texts, training_labels, validation_labels = train_test_split(training_texts, training_labels, test_size=.2)
+
+    import torch
+    from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    print("tokenizing...")
+    training_encodings = tokenizer(training_texts, truncation=True, padding=True)
+    validation_encodings = tokenizer(validation_texts, truncation=True, padding=True)
+    testing_encodings = tokenizer(testing_texts, truncation=True, padding=True)
+
+    print("datasets...")
+    training_dataset = BooksDataset(training_encodings, training_labels)
+    validation_dataset = BooksDataset(validation_encodings, validation_labels)
+    testing_dataset = BooksDataset(testing_encodings, testing_labels)
 
     training_args = TrainingArguments(
-        output_dir="models/",
-        overwrite_output_dir=True,
-        per_device_train_batch_size=64,
-        per_device_eval_batch_size=64,
-        num_train_epochs=100,
-        save_steps=10_000,
-        save_total_limit=2,
-        logging_dir='./logs',
+        output_dir="./models",
+        num_train_epochs=3,              # total number of training epochs
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=64,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+        logging_steps=10,
     )
-    
+    model = GPT2LMHeadModel.from_pretrained('gpt2-medium', pad_token_id = tokenizer.eos_token_id)
     trainer = Trainer(
         model=model,
         args=training_args,
-        data_collator=data_collator,
-        train_dataset=training_dataset,
-        eval_dataset=validation_dataset,
+        train_dataset=training_dataset,         # training dataset
+        eval_dataset=validation_dataset             # evaluation dataset
     )
     print("training...")
     trainer.train()
@@ -116,8 +126,9 @@ def generate_finetuned_response(query):
 def generate_default_response(query):
     import torch
     from transformers import GPT2LMHeadModel, GPT2Tokenizer
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-    model = GPT2LMHeadModel.from_pretrained('gpt2-large', pad_token_id = tokenizer.eos_token_id)
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+    print(f"{tokenizer.pad_token},{tokenizer.pad_token_id}")
+    model = GPT2LMHeadModel.from_pretrained('gpt2-medium', pad_token_id = tokenizer.eos_token_id)
 
     inputs = tokenizer.encode(query, return_tensors='pt')
     outputs = model.generate(inputs,
@@ -126,27 +137,26 @@ def generate_default_response(query):
         no_repeat_ngram_size = 2,
         early_stopping = True,
         do_sample=True,
-        temperature = 0.25,
+        temperature = 0.2,
         top_k=20)
     
     text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     print(text)
 
-def main(arg):
+def main(arg1,arg2=None):
     print("System starting...")
-    print(arg)
     exit_list = ["q","quit","q()","quit()","exit","exit()"]
-    if arg == 1:
-        print("test")
-        read_datasets()
-        # train_bot()
+    if arg1=="1":
+        if arg2=="1":
+            texts, labels = read_datasets()
+            train_bot(texts, labels)
         # while True:
         #     query = input()
         #     if query in exit_list: break
         #     generate_finetuned_response(query)
         # print("Exiting...")
 
-    elif arg == 0:
+    elif arg1=="0":
         print("System ready. Please enter your query: ")
         while True:
             query = input()
@@ -157,8 +167,7 @@ def main(arg):
 
 if __name__ == "__main__":
     import sys
-    if(len(sys.argv)>1):
-        print(sys.argv[1])
-        if sys.argv[1] in {0,1}: print("test"), main(sys.argv[1])
-        else: print("test2"), main(0)
-    else: main(0)
+    if(len(sys.argv)==3):
+        if sys.argv[1]=="1": main("1",sys.argv[2])
+        else: print("test"), main("0")
+    else: main("0")
