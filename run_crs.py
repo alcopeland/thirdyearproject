2cfedf00fcbca91ad77f5e6b34ba201110c9836c
alcopeland <a.l.copeland@lancaster.ac.uk>
2023-11-20 21:44:09 +0000

    Made progress with converting the data into the correct form, still not working. Also looked at a basic version of contextual awareness by keeping a log of the conversation history

--- run_crs.py
+++ run_crs.py
@@ -12,73 +12,95 @@ class BooksDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+def clean_text(text):
+    from nltk.tokenize import word_tokenize
+    tokenizedText = word_tokenize(text)
+
+    from nltk.corpus import stopwords
+    stop_words = set(stopwords.words('english'))
+    cleanText = []
+    for word in tokenizedText:
+        if word not in stop_words:
+            cleanText.append(word)
+    cleanTextS = ' '.join(cleanText)
+    return cleanTextS
+
 def read_booksummaries(texts, labels):
-    print("reading booksummaries")
+    print("reading booksummaries...")
     import re
     with open("datasets/uncompressed/booksummaries.txt", "r", encoding="utf-8") as file:
         sep1 = re.compile(r'"/m/\w{4,7}":')
         sep2 = re.compile(r'"/m/\w{4,7}"')
         sep3 = re.compile(r'/m/\w{4,7}')
+        sep4 = re.compile(r'=====')
         for line in file.readlines():
             newLine = re.sub(sep1,'',line)
             newLine = re.sub(sep2,'',newLine)
             newLine = re.sub(sep3,'',newLine)
+            newLine = re.sub(sep4,'',newLine)
             split = newLine.split("{")
             noNumber = split[0].split("\t\t")
             if(len(split)>1):
                 genreSplit = split[1].split("}")
                 text = ' '.join((noNumber[1] + genreSplit[1]).split())
-                texts.append(text)
+                cleanText = clean_text(text)
+                texts.append(cleanText)
                 label = re.sub('"','',genreSplit[0])
                 label = label.split(",  ")
                 label[0] = label[0][1:]
                 labels.append(label)
 
 def read_booksdataset(texts, labels):
-    print("reading booksdataset")
+    print("reading booksdataset...")
     import csv
-    with open("datasets/uncompressed/BooksDataSet.csv", "r", encoding="utf-8", errors="ignore") as file, open("datasets/uncompressed/total_text.txt", "a+", encoding="utf-8") as file2, open("datasets/uncompressed/cleanbooksdataset.txt", "w", encoding="utf-8") as cleanFile:
+    with open("datasets/uncompressed/BooksDataSet.csv", "r", encoding="utf-8", errors="ignore") as file:
         csv_reader = csv.reader(file)
         next(file)
         for row in csv_reader:
             text = f"{row[2]} {row[4]}"
-            texts.append(text)
-            labels.append(row[3])
+            cleanText = clean_text(text)
+            texts.append(cleanText)
+            labels.append([row[3]])
+
+def write_to_csv(texts, labels):
+    print("writing to csv...")
+    combinedArray = list(zip(texts,labels))
+    import csv
+    with open('final_text.csv', 'w+', encoding="utf-8", errors="ignore", newline='') as file:
+        writer = csv.writer(file)        
+        writer.writerow(["text","label"])
+        writer.writerows(combinedArray)
 
 def read_datasets():
     texts = []
     labels = []
-    print("reading datasets")
-    # read_booksummaries(texts, labels)
+    print("reading datasets...")
+    read_booksummaries(texts, labels)
     read_booksdataset(texts, labels)
     return texts, labels  
 
-def train_bot(texts, labels):
+def train_bot():
     print("training model")    
-    ratio = int(len(texts)*0.8)
-    training_texts = texts[:ratio]
-    testing_texts = texts[ratio:]
-    training_labels = labels[:ratio]
-    testing_labels = labels[ratio:]
-    
+    from datasets import load_dataset
+    dataset = load_dataset('csv', data_files = 'final_text.csv')
+
     from sklearn.model_selection import train_test_split
-    training_texts, validation_texts, training_labels, validation_labels = train_test_split(training_texts, training_labels, test_size=.2)
+    splitDataset = dataset['train'].train_test_split(test_size=0.2)
+    splitDataset = splitDataset.map(tokenize, batched=True, batch_size=None)
+
+    print(splitDataset["train"]["text"][0])
+    print(type(splitDataset["train"]["text"][0]))
+    print(splitDataset["train"]["label"][0])
+    print(type(splitDataset["train"]["label"][0]))
+    # print(splitDataset["train"]["label"][0][0])
+    # print(type(splitDataset["train"]["label"][0][0]))
+
 
     import torch
-    from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+    from transformers import TrainingArguments, Trainer, GPT2LMHeadModel, GPT2Tokenizer
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.pad_token_id = tokenizer.eos_token_id
-
-    print("tokenizing...")
-    training_encodings = tokenizer(training_texts, truncation=True, padding=True)
-    validation_encodings = tokenizer(validation_texts, truncation=True, padding=True)
-    testing_encodings = tokenizer(testing_texts, truncation=True, padding=True)
-
-    print("datasets...")
-    training_dataset = BooksDataset(training_encodings, training_labels)
-    validation_dataset = BooksDataset(validation_encodings, validation_labels)
-    testing_dataset = BooksDataset(testing_encodings, testing_labels)
 
     training_args = TrainingArguments(
         output_dir="./models",
@@ -94,8 +116,8 @@ def train_bot(texts, labels):
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=training_dataset,         # training dataset
-        eval_dataset=validation_dataset             # evaluation dataset
+        train_dataset=splitDataset["train"],         # training dataset
+        eval_dataset=splitDataset["test"]            # evaluation dataset
     )
     print("training...")
     trainer.train()
@@ -103,6 +125,29 @@ def train_bot(texts, labels):
 
     trainer.save_model("models/")
     tokenizer.save_pretrained("models/")
+
+def tokenize(batch):
+        import torch
+        from transformers import GPT2Tokenizer
+        tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        return tokenizer(batch["text"], padding=True, truncation=True)
+    
+def convert_labels(labels, listLabels):
+    indexList = []
+    for row in labels:
+        # rowList = []
+        # for word in row:
+        #     rowList.append(listLabels.index(word))
+        # indexList.append(rowList)
+        indexList.append(listLabels.index(row[0]))
+    return indexList
+
+def unique_labels_list(labels):
+    uniqueLabels = set(item for list in labels for item in list)
+    listLabels = list(uniqueLabels)
+    return listLabels
 
 def generate_finetuned_response(query):
     import torch
@@ -127,7 +172,6 @@ def generate_default_response(query):
     import torch
     from transformers import GPT2LMHeadModel, GPT2Tokenizer
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
-    print(f"{tokenizer.pad_token},{tokenizer.pad_token_id}")
     model = GPT2LMHeadModel.from_pretrained('gpt2-medium', pad_token_id = tokenizer.eos_token_id)
 
     inputs = tokenizer.encode(query, return_tensors='pt')
@@ -149,19 +193,24 @@ def main(arg1,arg2=None):
     if arg1=="1":
         if arg2=="1":
             texts, labels = read_datasets()
-            train_bot(texts, labels)
+            uniqueLabels = unique_labels_list(labels)
+            indexLabels = convert_labels(labels, uniqueLabels)
+            write_to_csv(texts,indexLabels)
+            train_bot()
         # while True:
         #     query = input()
         #     if query in exit_list: break
         #     generate_finetuned_response(query)
         # print("Exiting...")
-
     elif arg1=="0":
         print("System ready. Please enter your query: ")
+        convHistory = ""
         while True:
             query = input()
             if query in exit_list: break
-            generate_default_response(query)
+            generate_default_response(convHistory +" "+ query)
+            convHistory = convHistory +" "+ clean_text(query)
+            print(convHistory)
         print("Exiting...")
             
 
