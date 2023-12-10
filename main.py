3dde72fb6ab0d9dd3a0620d1bd202f5bb9bfac46
alcopeland <a.l.copeland@lancaster.ac.uk>
2023-12-10 16:53:00 +0000

    Completed a basic version of the program. It will take user input, classify it into a genre, then find the most similar summary using bert score for that genre. Outputs the top 10 matches.

--- main.py
+++ main.py
@@ -47,19 +47,51 @@ def load_dataset_from_csv():
     dataset = dataset['train'].train_test_split(test_size=0.2)
     return dataset
 
-def trainNaiveBayes(dataset):
+def train_naive_bayes(dataset):
     print("training classifier...")
-    from sklearn.naive_bayes import GaussianNB
+    # https://www.turing.com/kb/document-classification-using-naive-bayes
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.naive_bayes import MultinomialNB
+    from sklearn.pipeline import make_pipeline    
     from sklearn.metrics import accuracy_score
-    gnb = GaussianNB()
-    # Train the classifier:
+    
+    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    model.fit(dataset["train"]["text"],dataset["train"]["label"])
+    predicted_labels = model.predict(dataset["test"]["text"])
+
+    #print(predicted_labels)
+    #print(accuracy_score(dataset["test"]["label"], predicted_labels))
+    return model
+
+def predict_user_input(model):
+    print("Please enter a sentence: ")
+    query = input()
+    prediction = model.predict([query])
+    return query, prediction[0]
+
+def limit_dataset(prediction, dataset):
+    print("limiting dataset...")
+    refined_dataset = dataset.filter(lambda record: record["label"]==prediction)
+    # print(refined_dataset["train"]["name"])
+    print(len(refined_dataset["train"]["name"]))
+    return refined_dataset
+
+def calculate_bert_score(query, summary):
+    from bert_score import BERTScorer
+    scorer = BERTScorer(model_type='distilbert-base-uncased')
+    P, R, F1  = scorer.score([query], [summary])
+    return F1.detach().numpy()[0]
+
+def calculate_bert_scores(query, dataset):
+    print("calculating bert scores...")
     import numpy as np
-    model = gnb.fit(np.array(dataset["train"]["text"]).reshape(-1, 1), np.array(dataset["train"]["label"]))
-    # Make predictions with the classifier:
-    predictive_labels = gnb.predict(dataset["test"]["text"])
-    print(predictive_labels)
-    # Evaluate label (subsets) accuracy:
-    print(accuracy_score(dataset["test"]["label"], predictive_labels))
+    queries = np.repeat(query,len(dataset["train"]["text"]))
+    scores = map(calculate_bert_score, queries, dataset["train"]["text"])
+    scores = list(scores)
+    print(scores)
+    top_10_index = np.argsort(scores)[-10:]
+    print(top_10_index)
+    return top_10_index, scores
 
 def main():
     print("System starting...")
@@ -68,8 +100,15 @@ def main():
         names, labels, texts = read_datasets()
         write_to_csv(names, labels, texts)
     dataset = load_dataset_from_csv()
-    print(dataset)
-    trainNaiveBayes(dataset)
+    model = train_naive_bayes(dataset)
+    query, prediction = predict_user_input(model)
+    refined_dataset = limit_dataset(prediction, dataset)
+    top_10_index, all_scores = calculate_bert_scores(query, refined_dataset)
+    import numpy as np
+    top_10_names = list(np.array(refined_dataset["train"]["name"])[top_10_index])
+    print("Here are some similar books you might like: ")
+    for name in top_10_names:
+        print(f"- {name}")
 
 if __name__ == "__main__":
     main()
