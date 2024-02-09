afccf934ecca0f35ee5d27711fcfe212dbf53c06
alcopeland <99360302+alcopeland@users.noreply.github.com>
2024-02-09 11:29:52 +0000

    Added a shell script to set up the system

---
+++ setup.sh
@@ -0,0 +1,13 @@
+export PATH="/home/copelaal/.local/bin:$PATH"
+pip install datasets
+pip install sklearn
+pip install spacy
+python3 -m spacy download en_core_web_sm
+pip install nltk
+pip install transformers
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install bert_score
+pip install keybert
+
+# import nltk
+# nltk.download('all')
