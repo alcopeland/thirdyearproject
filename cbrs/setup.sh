138860ac44f688fea87a8cf700ee830af5be0810
alcopeland <a.l.copeland@lancaster.ac.uk>
2024-02-23 16:00:17 +0000

    changed the setup file to remove unneeded lines

--- cbrs/setup.sh
+++ cbrs/setup.sh
@@ -1,11 +1,9 @@
-export PATH="/home/copelaal/.local/bin:$PATH"
 pip install datasets
 pip install sklearn
 pip install spacy
 python3 -m spacy download en_core_web_sm
 pip install nltk
 pip install transformers
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install bert_score
 pip install keybert
 python3 -c 'import nltk; nltk.download("all")'
