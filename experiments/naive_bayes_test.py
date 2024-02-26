e2d70bc1c16751e38a12f886b1063b857c6ab862
alcopeland <a.l.copeland@lancaster.ac.uk>
2024-02-26 22:42:27 +0000

    Fixed the Naive Bayes test program to actually work as intended and re-added train_classifier as it was needed

--- experiments/naive_bayes_test.py
+++ experiments/naive_bayes_test.py
@@ -1,19 +1,19 @@
-from cbrs.train_classifier import load_dataset_from_csv, test_naive_bayes
+from train_classifier import load_dataset_from_csv, test_naive_bayes
 import concurrent.futures
 
 # A short program I created to test the accuracy of the Naive Bayes calssifier over multiple tests, using multiple test-train split values
 
-def test(sample: list, size: int) -> float:
+def test(sample: float, size: int) -> float:
     print(f"Starting {sample}...")
     scores = []
-    dataset = load_dataset_from_csv(size=sample)[1]
-    for i in range(1,size,1):
+    for i in range(0,size,1):
+        dataset = load_dataset_from_csv(size=sample)[1]
         scores.append(test_naive_bayes(dataset))
     return sum(scores)/len(scores)
 
 def main(): 
-    size = 10
-    sample = [0.05,0.1,0.15,0.2,0.25,0.3]
+    size = 1000
+    sample = [0.1,0.15,0.2,0.25]
     scores = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=min(len(sample), 10)) as executor:
         future_scores = [executor.submit(test, s, size) for s in sample]
